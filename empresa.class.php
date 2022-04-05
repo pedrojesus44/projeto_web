e2d3153739d0ea44395bc97894962dcc50baf4ab
pedrojesus44 <pedro.jesus44@etec.sp.gov.br>
2022-04-05 13:24:06 -0300

    segunda versao

---
+++ empresa.class.php
@@ -0,0 +1,18 @@
+<?php
+
+    class Empresa
+    {
+        private $razao_social;
+
+        public function setRazaoSocial($razao_social)
+        {
+            $this->razao_social = $razao_social;
+        }
+
+        public function getRazaoSocial()
+        {
+            return $this->razao_social;
+        }
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 
 require_once("pessoa.class.php");
+require_once("empresa.class.php");
 
 $pessoa = new Pessoa();
 $pessoa->setNome("Pedro Henrique Santos de Jesus");
 echo $pessoa->getNome();
+
+$empresa = new Empresa();
+$empresa->setRazaoSocial("Escola XPTO LTDA");
+echo $empresa->getRazaoSocial();
 
 ?>
